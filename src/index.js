6ce62c5aee9c3beba612ed5b0c80f9c83429b75c
German <bz5h7@yandex.ru>
2021-03-01 19:07:13 +0300

    feat: implement function to mirror digits in a given number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let number = Math.abs(n); // tests imply reversed number has no sign
+    let reverse = 0;
+    while (number > 0) {
+        lastDigit = number % 10;
+        reverse = (reverse * 10) + lastDigit;
+        number = Math.floor(number / 10);
+    }
+    return reverse;
 }
